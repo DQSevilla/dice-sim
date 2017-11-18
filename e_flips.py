e6449d5cbc80db1cb37662204cce13a46dc81770
ngoldstein51 <ngoldstein51@gmail.com>
2017-11-18 12:41:05 -0500

    QOL changes

--- e_flips.py
+++ e_flips.py
@@ -1,11 +1,20 @@
 def findeflips():
 	file = open("minflips","r") 
 	strings=file.readlines()
+	found=0
 	for i in strings:
-		if "$2:" in i:
-			print(float(i.split(":")[2]))
+		if "$4:" in i:
+			if i.split(":")[2] is "":
+				print("found but empty")
+				print("calling function to fill empty spaces with data")
+			else:
+				print(float(i.split(":")[2]))
+			found=1
 			break
-	
+	if found is 0:
+		print("not there")
+		print("calling function to fill text file to that point, then")
+		print("calling function to fill empyt spaces with data")
 	file.close()
 
 findeflips()
