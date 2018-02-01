b7045a73fd03fe4b77afe02c4972d6d06397645d
ngoldstein51 <ngoldstein51@gmail.com>
2018-02-01 16:30:05 -0500

    Fix expectation algorithm

--- e_flips.py
+++ e_flips.py
@@ -1,31 +1,48 @@
 #program to, given a number n representing the number of sides of a die, gives the expected number of coin flips needed
 #to simulate rolling said n-sidded die
 
-def e_flips_sim(n):
-	i=n
-	while not isPow2(i):
-		i=i+1
+memoize={1:0}
 
-	if n==2:
-		return 1
+def e_flips_sim(n):
+
+	i=2**(n - 1).bit_length()
+
+	if n in memoize:
+		return memoize[n]	
+
+	# if n==2:
+	# 	return 1
+
+	#print(str(n))
 
 	if n%2 == 0:
 		return 1+e_flips_sim(n//2)
 	else:
 		l=float('inf')
-		for m in range(n+1,i+1):
-			if l>((m/n)*e_flips_sim(m)):
-				l=((m/n)*e_flips_sim(m))
+		for m in range(n+1,n+2):
+			res=((m/n)*e_flips_sim(m))
+			if l>res:
+				l=res
 		return l
 
+def helper(i):
+	return memoization(i,e_flips_sim(i))
+
+def memoization(key,value):
+	memoize[key]=value
+	return value
+
 def isPow2(i):
-	if i<=1:
-		if i==1:
-			return 1
-		else:
-			return 0
+	return (i and (not(i&(i-1))))
 
-	return isPow2(i/2)
+for i in range(2,10001):
+	print(str(i)+" -> "+str(helper(i)))
+	print(memoize[i])
+	print("\n")
 
-for i in range(2,100):
-	print(str(i)+" -> "+str(e_flips_sim(i)))
+# for i in range(70,101):
+# 	print(str(i)+" -> "+str(helper(i)))
+# 	print(memoize[i])
+# 	print("\n")
+
+print(memoize)
