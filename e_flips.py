80b5f1122c2453907c149fb0bcac184bc469b3a4
ngoldstein51 <ngoldstein51@gmail.com>
2018-02-02 02:45:19 -0500

    shorten e_flips

--- e_flips.py
+++ e_flips.py
@@ -5,44 +5,22 @@ memoize={1:0}
 
 def e_flips_sim(n):
 
-	i=2**(n - 1).bit_length()
-
 	if n in memoize:
-		return memoize[n]	
-
-	# if n==2:
-	# 	return 1
-
-	#print(str(n))
+		return memoize[n]
 
 	if n%2 == 0:
 		return 1+e_flips_sim(n//2)
 	else:
-		l=float('inf')
-		for m in range(n+1,n+2):
-			res=((m/n)*e_flips_sim(m))
-			if l>res:
-				l=res
-		return l
+		return (((n+1)/n)*e_flips_sim(n+1))
 
 def helper(i):
-	return memoization(i,e_flips_sim(i))
+	val=e_flips_sim(i)
+	memoize[i]=val
+	return val
 
-def memoization(key,value):
-	memoize[key]=value
-	return value
-
-def isPow2(i):
-	return (i and (not(i&(i-1))))
-
-for i in range(2,10001):
+for i in range(2,101):
 	print(str(i)+" -> "+str(helper(i)))
 	print(memoize[i])
 	print("\n")
-
-# for i in range(70,101):
-# 	print(str(i)+" -> "+str(helper(i)))
-# 	print(memoize[i])
-# 	print("\n")
 
 print(memoize)
