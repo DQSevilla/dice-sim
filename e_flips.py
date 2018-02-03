7bbc59de2b2cff25b7379c2909d40b1bb337d718
ngoldstein51 <ngoldstein51@gmail.com>
2018-02-03 03:10:48 -0500

    e_flips take into account all factors

--- e_flips.py
+++ e_flips.py
@@ -1,6 +1,12 @@
 #program to, given a number n representing the number of sides of a die, gives the expected number of coin flips needed
 #to simulate rolling said n-sidded die
-exec(compile(open('app.py').read()))
+
+def all_factors(num):
+    ret=[]
+    for i in range(2,num):
+        if(num%i==0):
+            ret.append(i)
+    return ret
 
 memoize={1:0}
 
@@ -12,20 +18,27 @@ def e_flips_sim(n):
 	if n%2 == 0:
 		return 1+e_flips_sim(n//2)
 	else:
-		return (((n+1)/n)*flip_helper(n+1))
+		factors=all_factors(n)
+		poss_min=(((n+1)/n)*flip_helper(n+1))
+		
+		for i in factors:
+			val=(flip_helper(i)+flip_helper(n//i))
+			if poss_min>val:
+				poss_min=val
+		return poss_min
 
 def flip_helper(i):
 	val=e_flips_sim(i)
 	memoize[i]=val
 	return val
 
-for i in range(2,101):
-	flip_helper(i)
-	print(str(i)+" -> "+str(flip_helper(i)))
-	#print(memoize[i])
-	print("\n")
+for i in range(1,21):
+	var=flip_helper(i)
+	print(str(i)+" -> "+str(var))
+#print(memoize[i])
+print("\n")
 
 #f_out=open("lot_of_flips.txt","w+")
 #f_out.write(str(memoize))
 #f_out.close()
-print(memoize)
+#print(memoize)
